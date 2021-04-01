fbe2c4057e083065586b507c5b2b797ea67596c1
Anthony Pernia <anthonyperniah@gmail.com>
2021-04-01 15:33:38 -0300

    add json data

---
+++ script/script.js
@@ -0,0 +1,12 @@
+var json = require('../data/data.json');
+console.log(json);
+
+
+function set_data() {
+    var json = require('../data/data.json');
+    console.log(json);
+}
+
+
+
+document.addEventListener("DOMContentLoaded", set_data());
